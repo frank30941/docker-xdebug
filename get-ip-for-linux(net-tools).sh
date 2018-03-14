ef6baace44cfb66333240a82060266a448175e2a
DESKTOP-175J92E\frank30941 <frank30941@gmail.com>
2018-03-15 00:57:57 +0800

    rm file
    don't need.

--- get-ip-for-linux(net-tools).sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-IP_ADDRESS=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-echo "xdebug.remote_host=$IP_ADDRESS">>./php7-xdebug/xdebug.ini
--- get-ip-for-windows10.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SET mypath=%~dp0
-FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do set localIp=%%i
-
-echo xdebug.remote_host=%localIp%>> .\php7-xdebug\xdebug.ini
